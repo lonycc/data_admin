250321f8094b54c49c8aaab764e74fafbdc256b4
tony <xcc2533919@163.com>
2018-02-27 17:28:03 +0800

    Update Test.php

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,19 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Test extends CI_Controller {
-
-	public function __construct()
-	{
-		parent::__construct();
-		date_default_timezone_set('PRC');
-	}
-
-	// ------------------------------------------------------------------------
-	public function index()
-	{
-		$keyword = $this->input->get('keyword', TRUE);
-		echo  json_encode(['status'=>200, 'msg'=>'OK', 'data'=>htmlspecialchars($keyword)]);
-	}
-
+    public function __construct() {
+        parent::__construct();
+        date_default_timezone_set('PRC');
+    }
+    // ------------------------------------------------------------------------
+    public function index() {
+        $keyword = $this->input->get('keyword', TRUE);
+        echo json_encode(['status' => 200, 'msg' => 'OK', 'data' => htmlspecialchars($keyword) ]);
+    }
 }
