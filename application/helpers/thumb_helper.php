7cc62159fef4f65e90bd720031001debf7d6b814
tony <xcc2533919@163.com>
2018-02-27 17:32:33 +0800

    Update thumb_helper.php

--- application/helpers/thumb_helper.php
+++ application/helpers/thumb_helper.php
@@ -1,23 +1,18 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-if (!function_exists('create_thumb_preferences'))
-{
-    function create_thumb_preferences(& $data)
-    {
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+if (!function_exists('create_thumb_preferences')) {
+    function create_thumb_preferences(&$data) {
         if ($data['hasattach']) {
             $thumb_preferences = [];
             $thumb_preferences['enabled'] = [];
             $thumb_preferences['default'] = 'original';
             $thumbnails = CI()->input->post('thumbnail', TRUE);
-            if ( is_array($thumbnails) and count($thumbnails) > 0)
-            {
+            if (is_array($thumbnails) and count($thumbnails) > 0) {
                 foreach ($thumbnails as $thumbnail => $_e) {
-                    $thumb_preferences['enabled'][] = (String) $thumbnail;
+                    $thumb_preferences['enabled'][] = (String)$thumbnail;
                 }
-                $default = (String) CI()->input->post('thumb_default');
-                if ( $default and in_array($default, $thumb_preferences['enabled']) )
-                {
+                $default = (String)CI()->input->post('thumb_default');
+                if ($default and in_array($default, $thumb_preferences['enabled'])) {
                     $thumb_preferences['default'] = CI()->input->post('thumb_default', true);
                 }
             }
