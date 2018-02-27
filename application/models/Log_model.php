d0bfc068a655593a6559ba73290478da9cec2936
tony <xcc2533919@163.com>
2018-02-27 17:42:57 +0800

    Update Log_model.php

--- application/models/Log_model.php
+++ application/models/Log_model.php
@@ -1,30 +1,22 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Log_model extends CI_Model
-{
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-      /**
-       * 获取指定日志
-       */
-      public function get_logs($limit, $offset = 0)
-      {
-            return $this->db->order_by('pdate', 'desc')->limit($limit, $offset)->get($this->db->dbprefix('log'))->result();
-      }
-
-
-	// ------------------------------------------------------------------------
-
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class Log_model extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+    }
+    /**
+     * 获取指定日志
+     */
+    public function get_logs($limit, $offset = 0) {
+        return $this->db->order_by('pdate', 'desc')->limit($limit, $offset)->get($this->db->dbprefix('log'))->result();
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 获取操作日志总条数
      *
      */
-	public function get_log_nums()
-	{
-		return $this->db->count_all_results($this->db->dbprefix('log'));
-	}
+    public function get_log_nums() {
+        return $this->db->count_all_results($this->db->dbprefix('log'));
+    }
 }
