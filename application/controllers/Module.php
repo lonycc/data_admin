2669c8a73e23d2a2d94d2a8e47866a9b28c24bfa
tony <xcc2533919@163.com>
2018-02-27 17:24:44 +0800

    Update Module.php

--- application/controllers/Module.php
+++ application/controllers/Module.php
@@ -1,34 +1,28 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Module extends MY_Controller
-{
-
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class Module extends MY_Controller {
     protected $plugin = null;
-
     /**
      * 构造函数
      *
      * @access  public
      * @return  void
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         $this->acl->detect_plugin_menus();
         //$this->initialize();
+        
     }
-
-    private function initialize()
-    {
+    private function initialize() {
         $plugin = $this->input->get('plugin', TRUE);
-        if ( ! $plugin AND $this->acl->_default_link) {
+        if (!$plugin AND $this->acl->_default_link) {
             redirect($this->acl->_default_link);
         }
         $this->_check_permit();
         $controller = $this->input->get('c', TRUE);
         $method = $this->input->get('m', TRUE);
-        $path = APPPATH.'../plugins/'.$plugin.'/controllers/'.$plugin.'_'.$controller.'.php';
+        $path = APPPATH . '../plugins/' . $plugin . '/controllers/' . $plugin . '_' . $controller . '.php';
         if ($controller and file_exists($path)) {
             include $path;
             $controller = ucfirst($plugin . '_' . $controller);
@@ -40,9 +34,7 @@ class Module extends MY_Controller
             $this->_message('未找到处理程序!', '', FALSE);
         }
     }
-
-    public function run()
-    {
+    public function run() {
         $this->initialize();
     }
 }
