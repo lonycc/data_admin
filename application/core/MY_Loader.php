ef0febedd3240ee5283e869e025e783a325c0901
tony <xcc2533919@163.com>
2018-02-27 17:30:08 +0800

    Update MY_Loader.php

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -1,24 +1,15 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class MY_Loader extends CI_Loader
-{
-
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-	// ------------------------------------------------------------------------
-	public function switch_theme($theme = 'default')
-	{
-		$this->_ci_view_paths = array(APPPATH . 'views/' . $theme . '/'	=> TRUE);
-	}
-
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class MY_Loader extends CI_Loader {
+    public function __construct() {
+        parent::__construct();
+    }
+    // ------------------------------------------------------------------------
+    public function switch_theme($theme = 'default') {
+        $this->_ci_view_paths = array(APPPATH . 'views/' . $theme . '/' => TRUE);
+    }
 }
-
-function CI()
-{
+function CI() {
     $CI = & get_instance();
     return $CI;
 }
