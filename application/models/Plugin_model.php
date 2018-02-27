2f48a8cb06f09463c9e13ff536a58ec0654fadb4
tony <xcc2533919@163.com>
2018-02-27 17:44:54 +0800

    Update Plugin_model.php

--- application/models/Plugin_model.php
+++ application/models/Plugin_model.php
@@ -1,37 +1,28 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Plugin_model extends CI_Model
-{
-
-	/**
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class Plugin_model extends CI_Model {
+    /**
      * 构造函数
      *
      * @access  public
      * @return  void
      */
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function __construct() {
+        parent::__construct();
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 获取所有插件
      *
      * @access  public
      * @return  object
      */
-	public function get_plugins()
-	{
-		return $this->db->select('id, name, title, author, link, active')
-						->get($this->db->dbprefix('plugins'))
-						->result();
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function get_plugins() {
+        return $this->db->select('id, name, title, author, link, active')->get($this->db->dbprefix('plugins'))->result();
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 检测插件名称是否可用
      *
@@ -39,13 +30,11 @@ class Plugin_model extends CI_Model
      * @param   string
      * @return  object
      */
-	public function check_plugin_name($name)
-	{
-		return $this->db->select('id')->where('name', $name)->get($this->db->dbprefix('plugins'))->row();
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function check_plugin_name($name) {
+        return $this->db->select('id')->where('name', $name)->get($this->db->dbprefix('plugins'))->row();
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 根据插件ID获取插件信息
      *
@@ -53,13 +42,11 @@ class Plugin_model extends CI_Model
      * @param   int
      * @return  object
      */
-	public function get_plugin_by_id($id)
-	{
-		return $this->db->where('id', $id)->get($this->db->dbprefix('plugins'))->row();
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function get_plugin_by_id($id) {
+        return $this->db->where('id', $id)->get($this->db->dbprefix('plugins'))->row();
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 添加新插件
      *
@@ -67,16 +54,14 @@ class Plugin_model extends CI_Model
      * @param   array
      * @return  bool
      */
-	public function add_plugin($data)
-	{
-            if(! is_dir(BASEPATH."../plugins/".$data['name'])){
-                mkdir(BASEPATH."../plugins/".$data['name'], 0777, true);
-            }
-		return $this->db->insert($this->db->dbprefix('plugins'), $data);
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function add_plugin($data) {
+        if (!is_dir(BASEPATH . "../plugins/" . $data['name'])) {
+            mkdir(BASEPATH . "../plugins/" . $data['name'], 0777, true);
+        }
+        return $this->db->insert($this->db->dbprefix('plugins'), $data);
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 根据插件ID修改插件
      *
@@ -85,16 +70,14 @@ class Plugin_model extends CI_Model
      * @param   array
      * @return  bool
      */
-	public function edit_plugin($plugin_id, $data)
-	{
-            if(! is_dir(BASEPATH."../plugins/".$data['name'])){
-                mkdir(BASEPATH."../plugins/".$data['name'], 0777, true);
-            }
-		return $this->db->where('id', $plugin_id)->update($this->db->dbprefix('plugins'), $data);
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function edit_plugin($plugin_id, $data) {
+        if (!is_dir(BASEPATH . "../plugins/" . $data['name'])) {
+            mkdir(BASEPATH . "../plugins/" . $data['name'], 0777, true);
+        }
+        return $this->db->where('id', $plugin_id)->update($this->db->dbprefix('plugins'), $data);
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 激活/卸载插件
      *
@@ -103,13 +86,11 @@ class Plugin_model extends CI_Model
      * @param   int
      * @return  bool
      */
-	public function active_plugins($id, $status = 1)
-	{
-		return $this->db->where_in('id', $id)->set('active', $status)->update($this->db->dbprefix('plugins'));
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function active_plugins($id, $status = 1) {
+        return $this->db->where_in('id', $id)->set('active', $status)->update($this->db->dbprefix('plugins'));
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 删除插件
      *
@@ -117,13 +98,11 @@ class Plugin_model extends CI_Model
      * @param   array
      * @return  bool
      */
-	public function del_plugin($ids)
-	{
-		return $this->db->where_in('id', $ids)->delete($this->db->dbprefix('plugins'));
-	}
-
-	// ------------------------------------------------------------------------
-
+    public function del_plugin($ids) {
+        return $this->db->where_in('id', $ids)->delete($this->db->dbprefix('plugins'));
+    }
+    // ------------------------------------------------------------------------
+    
     /**
      * 导出插件
      *
@@ -131,12 +110,10 @@ class Plugin_model extends CI_Model
      * @param   array
      * @return  void
      */
-	public function export_plugin($ids)
-	{
-		$plugins = $this->db->where_in('id', $ids)->get($this->db->dbprefix('plugins'))->result();
-		foreach ($plugins as $plugin)
-		{
-			$xml = '<?xml version="1.0" encoding="UTF-8"?>
+    public function export_plugin($ids) {
+        $plugins = $this->db->where_in('id', $ids)->get($this->db->dbprefix('plugins'))->result();
+        foreach ($plugins as $plugin) {
+            $xml = '<?xml version="1.0" encoding="UTF-8"?>
 						<root name="">
 							<plugin>
 								<name>' . $plugin->name . '</name>
@@ -149,13 +126,12 @@ class Plugin_model extends CI_Model
 								<access>' . $plugin->access . '</access>
 							</plugin>
 						</root>';
-			//当前仅支持在普通环境下导出插件安装XML文件
-			if (file_exists(BASEPATH . '../plugins/' . $plugin->name)) {
-				file_put_contents(BASEPATH . '../plugins/' . $plugin->name .'/plugin_' . $plugin->name . '_install.xml', $xml);
-			}
-		}
-	}
-
-	// ------------------------------------------------------------------------
-
+            //当前仅支持在普通环境下导出插件安装XML文件
+            if (file_exists(BASEPATH . '../plugins/' . $plugin->name)) {
+                file_put_contents(BASEPATH . '../plugins/' . $plugin->name . '/plugin_' . $plugin->name . '_install.xml', $xml);
+            }
+        }
+    }
+    // ------------------------------------------------------------------------
+    
 }
